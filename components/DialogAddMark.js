764088afbf10c0c71bdaffcc2b883ade1528c28d
atude <mozamel.anwary1@gmail.com>
2019-12-21 03:30:44 +1100

    added option to determine marks, relocated snackbar

--- components/DialogAddMark.js
+++ components/DialogAddMark.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Portal, Dialog, TextInput, Button, Subheading, Headline, Caption, Checkbox, } from 'react-native-paper';
+import { Portal, Dialog, TextInput, Button, Subheading, Headline, Caption, Checkbox, IconButton, Text, } from 'react-native-paper';
 import { StyleSheet, View, Picker } from 'react-native';
 
 import Colors from '../constants/Colors.js';
@@ -10,8 +10,11 @@ export default class DialogAddMark extends React.Component {
     dialogMarkValue: "",
     dialogMarkWeight: "",
     dialogMarkCourse: "",
+    dialogMarkCourseTotal: "",
     dialogMarkCourseIndex: 0,
     isWeightedMark: false,
+    determineFromCourse: false,
+    showHelpDialog: false,
   }
 
   setDialog = () => { 
@@ -51,11 +54,55 @@ export default class DialogAddMark extends React.Component {
     })
   }
 
+  handleWeightedMarkSwitch = () => {
+    this.setState({
+      isWeightedMark: !this.state.isWeightedMark,
+      determineFromCourse: false,
+    });
+  }
+
+  handleDetermineMarkSwitch = () => {
+    this.setState({
+      isWeightedMark: false,
+      determineFromCourse: !this.state.determineFromCourse,
+    });
+  }
+
+  handleHelpDialog = () => {
+    this.setState({showHelpDialog: !this.state.showHelpDialog})
+  }
+
+  setDeterminedMark = (courseTotal) => {
+    const { dialogMarkCourseIndex, } = this.state;
+    const { currentCourses, } = this.props;
+
+    const marks = currentCourses[dialogMarkCourseIndex].marks ?? [];
+    let remainingWeight = 100 - marks.reduce((a, b) => a + Number(b.weight), 0);
+    let finalAssessmentMark = courseTotal - marks.reduce((a, b) => a + (Number(b.weight) / 100) * Number(b.mark), 0);
+    finalAssessmentMark = finalAssessmentMark / remainingWeight * 100;
+
+    console.log(remainingWeight);
+    console.log(finalAssessmentMark);
+
+    if(marks.length) {
+      
+    }
+
+    if(finalAssessmentMark < 0) finalAssessmentMark = 0;
+    if(finalAssessmentMark > 100) finalAssessmentMark = 100;
+
+    this.setState({
+      dialogMarkValue: finalAssessmentMark.toString(),
+      dialogMarkWeight: remainingWeight,
+
+      dialogMarkCourseTotal: courseTotal,
+    });
+  };
+
   render() {
     let { isDialog, currentCourses } = this.props;
-    let { dialogMarkName, dialogMarkValue, dialogMarkWeight, 
-      dialogMarkCourse, isWeightedMark } = this.state;
-
+    let { dialogMarkName, dialogMarkValue, dialogMarkWeight, dialogMarkCourseTotal,
+      dialogMarkCourse, showHelpDialog, isWeightedMark, determineFromCourse } = this.state;
 
     return (
       <Portal>
@@ -64,14 +111,36 @@ export default class DialogAddMark extends React.Component {
           <Dialog.Content style={styles.dialogContainer}>
 
             <View style={{flexDirection: 'row', marginBottom: 4}}>
-              <Checkbox onPress={() => this.setState({isWeightedMark: !isWeightedMark})} 
+              <Checkbox onPress={() => this.handleWeightedMarkSwitch()} 
                 status={isWeightedMark ? 'checked' : 'unchecked'}>
               </Checkbox>
               <Subheading style={styles.checkboxText} 
-                onPress={() => this.setState({isWeightedMark: !isWeightedMark})}>
+                onPress={() => this.handleWeightedMarkSwitch()}>
                 Pre-weighted
               </Subheading>
             </View>
+
+            <View style={{flexDirection: 'row', marginBottom: 18, alignItems: "center"}}>
+              <Checkbox onPress={() => this.handleDetermineMarkSwitch()} 
+                status={determineFromCourse ? 'checked' : 'unchecked'}>
+              </Checkbox>
+              <Subheading style={styles.checkboxText} 
+                onPress={() => this.handleDetermineMarkSwitch()}>
+                Determine last assessment mark from course total
+              </Subheading>
+              <IconButton
+                icon="help"
+                color={showHelpDialog ? Colors.tintColor : Colors.grey}
+                onPress={() => this.handleHelpDialog()}
+              />
+            </View>
+
+            {showHelpDialog && 
+              <Text style={styles.helpDialogText}>
+                Use this option when you have the total course mark for a course, but have 
+                not been given the mark for the final assessment (usually for final exams).
+              </Text>
+            }
 
             <TextInput 
               style={styles.textInputFull}
@@ -81,25 +150,49 @@ export default class DialogAddMark extends React.Component {
               mode="outlined"
             />           
 
-            <View style={styles.inputContainer}>
-              <TextInput 
-                style={styles.textInputL}
-                label={!isWeightedMark ? "Mark (%)" : "Weighted Mark"} 
-                keyboardType={'numeric'}
-                value={(Number(dialogMarkValue) > 100 || dialogMarkValue.length > 5) ? dialogMarkValue.slice(0, 2) : dialogMarkValue}
-                onChangeText={dialogMarkValue => {this.setState({dialogMarkValue})}}
-                mode="outlined"
-              />
+            {determineFromCourse ? 
+              <View style={styles.inputContainer}>
+                <TextInput 
+                  disabled
+                  style={styles.textInputL}
+                  label={"Determined Mark (%)"} 
+                  keyboardType={'numeric'}
+                  value={(Number(dialogMarkValue) > 100 || dialogMarkValue.length > 5) ? dialogMarkValue.slice(0, 2) : dialogMarkValue}
+                  onChangeText={dialogMarkValue => {this.setState({dialogMarkValue})}}
+                  mode="outlined"
+                />
 
-              <TextInput 
-                style={styles.textInputR}
-                label={!isWeightedMark ? "Weighting (%)" : "Weight Total"}
-                value={(Number(dialogMarkWeight) > 100 || dialogMarkWeight.length > 5) ? dialogMarkWeight.slice(0, 2) : dialogMarkWeight}
-                onChangeText={dialogMarkWeight => {this.setState({dialogMarkWeight})}}
-                mode="outlined"
-                keyboardType={'numeric'}
-              />
-            </View>
+                <TextInput 
+                  style={styles.textInputR}
+                  label={"Course Total"}
+                  value={(Number(dialogMarkCourseTotal) > 100 || dialogMarkCourseTotal.length > 5) ? dialogMarkCourseTotal.slice(0, 2) : dialogMarkCourseTotal}
+                  onChangeText={courseTotal => this.setDeterminedMark(courseTotal)}
+                  mode="outlined"
+                  keyboardType={'numeric'}
+                />
+              </View>
+            :
+              <View style={styles.inputContainer}>
+                <TextInput 
+                  style={styles.textInputL}
+                  label={!isWeightedMark ? "Mark (%)" : "Weighted Mark"} 
+                  keyboardType={'numeric'}
+                  value={(Number(dialogMarkValue) > 100 || dialogMarkValue.length > 5) ? dialogMarkValue.slice(0, 2) : dialogMarkValue}
+                  onChangeText={dialogMarkValue => {this.setState({dialogMarkValue})}}
+                  mode="outlined"
+                />
+
+                <TextInput 
+                  style={styles.textInputR}
+                  label={!isWeightedMark ? "Weighting (%)" : "Weight Total"}
+                  value={(Number(dialogMarkWeight) > 100 || dialogMarkWeight.length > 5) ? dialogMarkWeight.slice(0, 2) : dialogMarkWeight}
+                  onChangeText={dialogMarkWeight => {this.setState({dialogMarkWeight})}}
+                  mode="outlined"
+                  keyboardType={'numeric'}
+                />
+              </View>
+            }
+         
           
             <Subheading style={styles.courseHeading}>Select Course</Subheading>
             <Picker
@@ -126,8 +219,11 @@ export default class DialogAddMark extends React.Component {
 
 const styles = StyleSheet.create({
   checkboxText: {
-    textAlign: 'right',
-    textAlignVertical: 'center'
+    textAlign: 'left',
+    textAlignVertical: 'center',
+    flex: 1,
+    marginLeft: 5,
+    fontSize: 15,
   },
   dialogContainer: {
 
@@ -159,4 +255,11 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     color: Colors.tintColor,
   },
+  helpDialogText: {
+    paddingBottom: 30,
+    paddingLeft: 30,
+    paddingRight: 30,
+
+    color: Colors.grey,
+  }
 });
