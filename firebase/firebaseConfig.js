85d5f4d78882cfa120dd399dcbe09b9c51f2a72d
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-02-01 02:16:01 +1100

    Update firebase config method

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,24 +1,6 @@
 import firebase from 'firebase';
 import '@firebase/firestore';
-import {
-  API_KEY,
-  AUTH_DOMAIN,
-  DATABASE_URL,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGE_SENDER_ID,
-  APP_ID,
-} from '../.env';
-
-const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  databaseURL: DATABASE_URL,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGE_SENDER_ID,
-  appId: APP_ID,
-};
+import firebaseConfig from '../.env.js';
 
 //Init firebase
 let getFirebase = firebase.initializeApp(firebaseConfig);
